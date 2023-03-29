ada7d29cc5f94185df9ade3a2af6830ccc55fe91
Mandoko <108988699+Mandoko@users.noreply.github.com>
2023-03-29 18:18:46 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_itel-W4002-user
-add_lunch_combo omni_itel-W4002-userdebug
-add_lunch_combo omni_itel-W4002-eng
+add_lunch_combo omni_W4002-user
+add_lunch_combo omni_W4002-userdebug
+add_lunch_combo omni_W4002-eng
